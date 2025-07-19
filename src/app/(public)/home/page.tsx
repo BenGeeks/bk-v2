f22510dfe4f4cbc22d737ba390940f3759c23d0e
Ben Gican II <b3nhur77@gmail.com>
2025-07-19 19:28:48 +0800

    fix auth option error

--- src/app/(public)/home/page.tsx
+++ src/app/(public)/home/page.tsx
@@ -3,7 +3,7 @@ import PageContainer from "../components/page-container";
 
 const HomePage = () => {
   return (
-    <PageContainer title="Home Page" imgUrl="https://img.freepik.com/free-photo/sunset-time-tropical-beach-sea-with-coconut-palm-tree_74190-1075.jpg?semt=ais_hybrid&w=740">
+    <PageContainer title="Bebeng's Kitchen" imgUrl="https://img.freepik.com/free-photo/sunset-time-tropical-beach-sea-with-coconut-palm-tree_74190-1075.jpg?semt=ais_hybrid&w=740">
       HomePage goes here
     </PageContainer>
   );
